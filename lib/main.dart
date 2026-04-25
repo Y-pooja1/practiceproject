dd1b3bd4098d39e1b83f5dff673146abca390732
Y-pooja1 <pooja.yr09@gmail.com>
2026-04-25 23:07:07 +0530

    pooja changed count text

--- lib/main.dart
+++ lib/main.dart
@@ -114,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Text(
-              '$_counter',
+              '----- $_counter --- ',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
