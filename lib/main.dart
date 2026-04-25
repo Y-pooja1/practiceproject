66c5ed335f3dcf35920dfc0835ff811a4b5310f7
adminshv <admin@softhealth.co.in>
2026-04-25 23:11:56 +0530

    vinay 4

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Oye Git Nerchkuntunna neetoh',
+              'Oye Git Nerchkuntunna neetoh \n Ok Nerchuko',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
